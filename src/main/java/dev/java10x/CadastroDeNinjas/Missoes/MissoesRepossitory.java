71b08db193dad58c639eaa45121e1f89b4f7024a
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2024-11-07 14:18:39 -0300

    feat: Criado a interface MissoesRepository e as classes NinjasRepossitory e NinjaService

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepossitory.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepossitory extends JpaRepository<MissoesModel, Long> {
+}
